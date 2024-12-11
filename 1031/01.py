c87712c7c7774cef319efd59cfbfc3c754da2213
Sejin Kang <ks320205@naver.com>
2024-12-11 18:02:00 +0900

    왜 노랭이로만 나오는지 모르겠네..

--- 1031/01.py
+++ 1031/01.py
@@ -1,25 +1,36 @@
-#이미지 히스토그램
+#이미지 히스토그램 (페이지 241)
 import cv2
 import numpy as np
 
 def draw_histo(hist, shape=(200,256)):
-    hist_img = np.full(shape, 255, np.uint8)
+    hist_img = np.full(shape, 255, np.uint8) # 흰색 배경 이미지 생성
     cv2.normalize(hist, hist, 0, shape[0], cv2.NORM_MINMAX) #정규화
-    gap = hist_img.shape[1]/hist.shape[0]
+    gap = hist_img.shape[1]/hist.shape[0]  # 막대의 너비 계산
     
-    for i, h in enumerate(hist):
+    for i, h in enumerate(hist):  # 히스토그램 값 반복
         x = int(round(i * gap)) #막대 사각형 시작 x좌표
-        w = int(round(gap))
-        cv2.rectangle(hist_img, (x, 0, w, int(h)), 0, cv2.FILLED)
+        w = int(round(gap)) # 막대의 너비
+        cv2.rectangle(hist_img, (x, 0, w, int(h)), 0, cv2.FILLED) # 히스토그램 막대 그리기
     
     return cv2.flip(hist_img, 0) #영상 상하 뒤집기 후 반환
 
-image = cv2.imread("../1017/minMax.jpg", cv2.IMREAD_GRAYSCALE)
+image = cv2.imread("color_model.jpg", cv2.IMREAD_GRAYSCALE)
 if image is None: raise Exception("영상 파일 읽기 오류")
 
 hist = cv2.calcHist([image], [0], None, [32], [0,256])
 hist_img = draw_histo(hist)
-
+# 히스토그램 이미지: 밝기 값의 빈도를 나타내는 막대 그래프
 cv2.imshow("image", image)
 cv2.imshow("hist_img",hist_img)
 cv2.waitKey(0)
+
+
+# cv2.calcHist:
+# 이미지의 히스토그램을 계산.
+# 입력 매개변수:
+# [image]: 입력 이미지.
+# [0]: 채널 0(그레이스케일).
+# None: 마스크 없음.
+# [32]: 히스토그램의 빈(bin) 개수 (32개의 구간으로 나눔).
+# [0,256]: 픽셀 값 범위 (0~255).
+# 반환값: 32개의 빈에 각 픽셀 값이 속한 빈도의 배열.
--- 1031/02.py
+++ 1031/02.py
@@ -1,6 +1,41 @@
-#색상 히스토그램2
+#색상 히스토그램 그리기
 import cv2
 import numpy as np
 
+# hue채널 팔레트 행렬 생성 함수
 def make_palette(rows):
-    hue = [round(i*180/rows)]
+    # 리스트 생성 방식
+    # hue = [round(i * 180 / rows) for i in range(rows)]
+    # hsv = [[[h, 255, 255]] for h in hue]
+    # hsv = np.array(hsv, np.uint8)
+
+    # 반복문 방식
+    hsv = np.full((rows, 1, 3), (255,255,255), np.uint8)
+    for i in range(0, rows):    # 행수만큼 반복
+        hue = round(i / rows * 180) # 색상 계산
+        hsv[i] = (hue, 255, 255)    #hsv 컬러 지정
+
+    return cv2.flip(hsv, 0)
+
+def draw_hist_hue(hist, shape=(200, 256, 3)):
+    hsv_palette = make_palette(hist.shape[0])
+    hist_img = np.full(shape, 255, np.uint8)
+    cv2.normalize(hist, hist, 0, shape[0], cv2.NORM_MINMAX)
+
+    gap = hist_img.shape[1] / hist.shape[0]
+    for i, h in enumerate(hist):
+        x,w = int(round(i * gap)), int(round(gap))
+        color = tuple(map(int, hsv_palette[i][0]))
+        cv2.rectangle(hist_img, (x, 0, w, int(h)), color, cv2.FILLED)
+
+    return cv2.flip(hist_img, 0)
+
+image = cv2.imread("hue_hist.jpg", cv2.IMREAD_COLOR)
+
+hsv_img = cv2.cvtColor(image, cv2.COLOR_BGR2HSV) #BGR을 HSV로 변환
+hue_hist = cv2.calcHist( [hsv_img], [0], None, [18], [0,180] )
+hue_hist_img = draw_hist_hue(hue_hist, (200, 360, 3))
+
+cv2.imshow("img", image)
+cv2.imshow("hue_hist_img",hue_hist_img)
+cv2.waitKey(0)
