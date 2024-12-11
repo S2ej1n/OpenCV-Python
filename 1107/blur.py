5cc662b8180a057e33629fb2e27a4774617f6699
Sejin Kang <ks320205@naver.com>
2024-12-11 20:30:34 +0900

    솔트페퍼와 블러들

--- 1107/blur.py
+++ 1107/blur.py
@@ -1,37 +1,49 @@
-#책 예제 - 블러링
+# 책 예제 - 블러링
+# 이미지 블러링을 직접 구현
 import numpy as np
 import cv2
 
-# 회선 수행 함수 - 행렬 처리 방식
+# 회선 수행 함수 - 행렬 처리 방식 , 속도가 빠르고 간결함
 def filter(image, mask):
-    rows, cols = image.shape[:2]
-    dst = np.zeros((rows, cols), np.float32)
-    mask_size = mask.shape[0] // 2  # 마스크의 절반 크기
+    rows, cols = image.shape[:2] # 입력 이미지 행, 열 크기 가져옴.
+    dst = np.zeros((rows, cols), np.float32) # 결과 이미지 저장할 배열
+    mask_size = mask.shape[0] // 2
+    # 마스크의 절반 크기 - 마스크를 중심으로 입력 이미지를 처리하기 때문
 
     # 입력 행렬 반복 순회
-    for i in range(mask_size, rows - mask_size):
-        for j in range(mask_size, cols - mask_size):
+    # 범위는 마스가 이미지 경계를 벗어나지 않도록 이렇게 설정함
+    for i in range(mask_size, rows - mask_size): #행부터
+        for j in range(mask_size, cols - mask_size): #열까지
             y1, y2 = i - mask_size, i + mask_size + 1  # 관심 영역 높이 범위
+            #행의 위아래
             x1, x2 = j - mask_size, j + mask_size + 1  # 관심 영역 너비 범위
-            roi = image[y1:y2, x1:x2].astype('float32')  # 관심 영역 형 변환
+            #열의 좌우
+
+            roi = image[y1:y2, x1:x2].astype('float32')
+            # 관심영역 만들고 형변환
+
             tmp = cv2.multiply(roi, mask)
+            # 관심영역과 마스크의 요소 곱하기
+
             dst[i, j] = cv2.sumElems(tmp)[0]
+            # tmp 배열의 모든 요소 합산, 현재 픽셀의 결과값 ㅖ산.
 
     return dst
 
 
-# 회선 수행 함수 - 화소 직접 근접 방식
+# 회선 수행 함수 - 화소 직접 근접 방식, 중첩 루프를 사용하여 마스크의 각 요소를 직접 접근하며 곱셈 연산을 수행
 def filter2(image, mask):
     rows, cols = image.shape[:2]
     dst = np.zeros((rows, cols), np.float32)
     mask_size = mask.shape[0] // 2  # 마스크의 절반 크기
 
     for i in range(mask_size, rows - mask_size):
-        for j in range(mask_size, cols - mask_size):
-            sum = 0.0
-            for u in range(mask.shape[0]):
+        for j in range(mask_size, cols - mask_size): #여기까진 동일
+            sum = 0.0 # 마스크와 관심 영역의 곱셈 결과를 누적할 변수
+            for u in range(mask.shape[0]): # mask.shape[0] 마스크의 행 크기
                 for v in range(mask.shape[1]):
-                    y, x = i + u - mask_size, j + v - mask_size
+                    y, x = (i + u - mask_size,
+                            j + v - mask_size)
                     sum += image[y, x] * mask[u, v]
             dst[i, j] = sum
     return dst
@@ -46,12 +58,14 @@ data = [1 / 9] * 9  # 3x3 평균 블러 마스크
 mask = np.array(data, np.float32).reshape(3, 3)
 
 # 필터 함수 적용
-blur1 = filter(image, mask)
-blur2 = filter2(image, mask)
+blur1 = filter(image, mask) #행렬
+blur2 = filter2(image, mask) #화소 직접 접근
 
 # 스케일 조정 및 타입 변환
 blur1 = cv2.normalize(blur1, None, 0, 255, cv2.NORM_MINMAX)
 blur1 = blur1.astype('uint8')
+
+
 blur2 = cv2.convertScaleAbs(blur2)
 
 # 결과 출력
--- 1107/salt_pepper_noise.py
+++ 1107/salt_pepper_noise.py
@@ -1,15 +1,24 @@
 import numpy as np, cv2
 
 def salt_pepper_noise(img, n):
-    h,w = img.shape[:2]
+    h,w = img.shape[:2] # 이미지 높이와 너비 가져오기
     x,y = np.random.randint(0, w, n), np.random.randint(0,h,n)
-    noise = img.copy()
-    for (x,y) in zip(x,y):
-        noise[y,x] = 0 if np.random.rand()<0.5 else 255
+    # 높이와 너비 사이의 랜덤 값 만듬
+    noise = img.copy() # 노이즈 추가할 이미지 생성
+
+    # x = [10, 20, 30]
+    # y = [1, 2, 3]
+    # list(zip(x, y))  # 결과: [(10, 1), (20, 2), (30, 3)]
+    for (x,y) in zip(x,y): #생성된 랜덤 좌표를 순회하며, 해당 좌표의 픽셀 값을 변경.
+        noise[y,x] = 0 if np.random.rand() < 0.5 else 255
+        # 0.5보다 작으면 해당 좌표 픽셀 값(noise[y, x])을 검정(0)으로 설정.
+        # 0.5보다 크면 해당 좌표 픽셀 값을 흰색(255)으로 설정.
     return noise
 
 image = cv2.imread("Lenna.png", cv2.IMREAD_GRAYSCALE)
+
 noise = salt_pepper_noise(image,500)
+
 dst = cv2.medianBlur(noise, 7) #7은 ksize = 커널 크기 = mask의 크기 = window
 #kseize를 올릴수록 더 좋아진다
 dst2 = cv2.GaussianBlur(noise, ksize=(7,7), sigmaX=10.0)
@@ -17,7 +26,7 @@ dst2 = cv2.GaussianBlur(noise, ksize=(7,7), sigmaX=10.0)
 dst3 = cv2.blur(noise, ksize=(7,7))
 
 cv2.imshow("noise", noise)
-cv2.imshow("dst", dst)
-cv2.imshow("dst2", dst2)
-cv2.imshow("dst3",dst3)
+cv2.imshow("medianBlur", dst)
+cv2.imshow("GaussianBlur", dst2)
+cv2.imshow("blur",dst3)
 cv2.waitKey(0)
