3e54d8ea957d560df5108132577e440e5e1ef4d6
Sejin Kang <ks320205@naver.com>
2024-12-12 21:22:02 +0900

    오 됐다 마우스드래그 회전

--- 1120/rotation.py
+++ 1120/rotation.py
@@ -1,3 +1,4 @@
+# 마우스 드래그로 영상 회전
 # 코드 수정해야함
 import math
 import numpy as np, cv2
@@ -7,9 +8,17 @@ def bilinear_value(img, pt):
     x, y = np.int32(pt)
     if x >= img.shape[1]-1: x = x - 1
     if y >= img.shape[0]-1: y = y - 1
+    P1, P2, P3, P4 = np.float32(img[y:y+2,x:x+2].flatten())
+    # 4개 화소 - 관심 영역으로 접근
+
+    alpha, beta = pt[1] - y, pt[0] - x
+    M1 = P1 + alpha * (P3 - P1)
+    M2 = P2 + alpha * (P4 - P2)
+    P = M1 + beta * (M2 - M1)
+    return np.clip(P, 0, 255)
 
 def contain(p, shape):
-    return 0 <= p[0] <shape[0] and 0 <= p[1] < shape[1]
+    return 0 <= p[0] < shape[0] and 0 <= p[1] < shape[1]
 
 # 예제 8.5.1
 def rotate_pt(img, degree, pt):
