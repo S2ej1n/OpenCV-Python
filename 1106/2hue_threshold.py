56a84ff8821dbf450c0f349ba8596729c0b8c96d
Sejin Kang <ks320205@naver.com>
2024-12-17 20:37:21 +0900

    이진화 컬러

---
+++ 1106/2hue_threshold.py
@@ -0,0 +1,35 @@
+# 이진화 책 예제, hue 채널을 이용한 객체 검출
+# 입력 영상을 컬러 HSV
+import numpy as np, cv2
+
+def onThreshold(value):
+    th[0] = cv2.getTrackbarPos("Hue_th1", "result") #상한값
+    th[1] = cv2.getTrackbarPos("Hue_th2", "result") #하한값
+
+    ## OpenCV 이진화 함수 이용 - 상위 값과 하위 값 제거
+    _, result = cv2.threshold(hue, th[1], 255, cv2.THRESH_TOZERO_INV)
+    # 상한값보다 크면 0, 작거나 같으면 그냥 유지.
+    cv2.threshold(result, th[0], 255, cv2.THRESH_BINARY, result)
+    # 하한값보다 크거나 같으면 255, 작으면 0
+
+    # 컬러로 바꾸려고 이거 추가
+    color_result = cv2.bitwise_and(BGR_img,BGR_img,mask=result)
+
+    cv2.imshow("result", color_result)
+    # => th0과 th1 사이의 값만 흰색 255로 남고, 나머지는 검정 0 으로 설정
+
+BGR_img = cv2.imread("color_space.jpg", cv2.IMREAD_COLOR)  # 컬러 영상 읽기
+if BGR_img is None:
+    raise Exception("영상파일 읽기 오류")
+
+HSV_img = cv2.cvtColor(BGR_img, cv2.COLOR_BGR2HSV)  # 컬러 공간 변환
+hue = np.copy(HSV_img[:, :, 0])  # hue 행렬에 색상 채널 복사
+
+th = [50, 100]  # 트랙바로 선택할 범위 변수
+cv2.namedWindow("result")
+cv2.createTrackbar("Hue_th1", "result", th[0], 255, onThreshold)
+cv2.createTrackbar("Hue_th2", "result", th[1], 255, onThreshold)
+onThreshold(th[0])  # 이진화 수행
+
+cv2.imshow("BGR_img", BGR_img)
+cv2.waitKey(0)
